c9efab44ef680b37746b7bf9873eebd0ec99278c
usagiloli <daimao2004@hotmail.com>
2021-06-17 15:53:04 +0800

    ocr_translater

--- usarot.py
+++ usarot.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+
+#Author:usagiloli
+'''ocr翻译软件，很难用，能跑起来，要装下边的包，还有tesseract的语言包，还要腾讯机翻的api，还要自己写一个cfg文件。pos3是鼠标静止1s取坐标'''
+
+
 from txtmt import txfanyi
 import cv2
 import numpy as np
@@ -9,7 +14,9 @@ from time import sleep
 import pynput
 import pytesseract
 import re
-import configparser
+import json
+#import configparser
+
 
 def press(x,y,but,p):
     if(p):
@@ -19,9 +26,24 @@ def unpress(x,y,but,p):
         return False
 
 def waitclick(fx=press):
-    with pynput.mouse.Listener(on_click=fx) as linster:
-        linster.join()
+    with pynput.mouse.Listener(on_click=fx) as listener:
+        listener.join()
 
+
+
+def keyonpress(key):
+    if (key == pynput.keyboard.Key.esc):
+        global flag
+        flag = 0
+        print("Click to exit")
+        return False
+    else:
+        pass
+def keyonrelease(key):
+        pass
+def keylisten():
+    esclistener=pynput.keyboard.Listener(on_press=keyonpress,on_release=keyonrelease)
+    esclistener.start()
 def getpos3(sample=1):
     while (1):
         pos1 = pag.position()
@@ -59,7 +81,10 @@ def getrgn(sample=1,mode='pos3'):
 
 
 def resformat(input):
-    input=re.sub('\\s+',' ',input)
+    input=re.sub(' +',' ',input)
+    input=re.sub('(\n|\r|\f)+','\n',input)
+    if(len(input)>0 and input[-1]=='\n'):
+        input=input[:-1]
     return input
 
 #res=pag.size()
@@ -89,18 +114,33 @@ def sltrgn(mode='pos3'):
     return rgn
 
 
-cf=configparser.ConfigParser()
+'''cf=configparser.ConfigParser()
 cf.read("usarot.cfg")
 mode = cf.get("cfg","mode")
 st1 = cf.get("cfg","id")
-st2 = cf.get("cfg","key")
+st2 = cf.get("cfg","key")'''
 
-rgn=sltrgn(mode)
+flag=1
+try:
+    with open("usarot.json",'r') as jsonf:
+        cfg=json.load(jsonf)
+        mode=cfg["mode"]
+        st1=cfg["id"]
+        st2=cfg["key"]
+except IOError:
+    with open("usarot.json", 'w') as jsonf:
+        cfg={"mode":"pos3","id":"","key":""}
+        json.dump(cfg,jsonf)
+        flag=0
+        print("Check json")
 
-while(1):
+if(flag):
+    rgn=sltrgn(mode)
+    keylisten()
+while(flag):
     imgcv=shotrgn(rgn)
     code=pytesseract.image_to_string(imgcv,lang='eng')
-    resformat(code)
+    code=resformat(code)
     print(code)
     fanyi=txfanyi(code,st1,st2)
     fanyi=resformat(fanyi)
